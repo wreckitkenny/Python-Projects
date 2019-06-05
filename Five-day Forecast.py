adfb2d520652a0d9baccf7416f69e900ee5cf40b
Wreck-it Kenny <wreckitkenny@users.noreply.github.com>
2019-06-05 16:10:35 +0700

    Update Five-day Forecast.py

--- Five-day Forecast.py
+++ Five-day Forecast.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python3
 import requests
 from datetime import datetime
+import argparse
+import sys
 
-api_key = "bUVmjeqq2QjIOEGH6uPGB0Any4ALGGvk"
-location = "hanoi"
+
+def main():
+    parser = argparse.ArgumentParser(description="5-day Weather Forecast Tool")
+    parser.add_argument("api_key", type=str, help="Accuweather API key")
+    parser.add_argument("location", type=str, help="City or Country")
+    args = parser.parse_args()
+    print("""====== Current Date & Time ======
+Time: {:%X}""".format(datetime.now()))
+    print("Weekday: {:%A}".format(datetime.now()))
+    print("Date: {:%d/%m/%Y}".format(datetime.now()))
+    if len(sys.argv) == 3:
+        if args.api_key and args.location:
+            sys.stdout.write(str(five_day_forecast(args)))
 
 
-def get_location_key(loc):
+def get_location_key(api_key, loc):
     _parameters = {"apikey": api_key, "q": loc}
     _url = "http://dataservice.accuweather.com/locations/v1/cities/search"
     _request = requests.get(_url, params=_parameters)
@@ -14,15 +27,16 @@ def get_location_key(loc):
     return(_info[0]["Key"])
 
 
-def five_day_forecast():
+def five_day_forecast(args):
     _num = 1
-    _parameters = {"apikey": api_key, "metric": "true"}
+    _parameters = {"apikey": args.api_key, "metric": "true"}
     _url = "http://dataservice.accuweather.com/forecasts/v1/daily/5day/\
-                                    {}".format(get_location_key(location))
+                                    {}".format(get_location_key(args.api_key,
+                                                                args.location))
     _request = requests.get(_url, params=_parameters)
     _info = _request.json()
     print("")
-    print("====== 5 Days of Daily Forecasts ======")
+    print("===== 5 Days of Daily Forecasts =====")
     for i in _info["DailyForecasts"]:
         _date = str(i["Date"][8:10]) + "/" + str(i["Date"][5:7]) + "/"\
                     + str(i["Date"][0:4])
@@ -32,12 +46,8 @@ def five_day_forecast():
     + Maximum Temperature: {}°C
     - Minimum Temperature: {}°C""".format(_num, _date, _maxtemp, _mintemp))
         _num += 1
+    return("=====================================")
 
 
 if __name__ == '__main__':
-    print("""
-====== Current Date & Time ======
-Time: {:%X}""".format(datetime.now()))
-    print("Weekday: {:%A}".format(datetime.now()))
-    print("Date: {:%d/%m/%Y}".format(datetime.now()))
-    five_day_forecast()
+    main()
